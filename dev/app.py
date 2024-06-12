3cbe130414e8a741941617157140a6e137d51ed6
Allison <hughesneal88@gmail.com>
2024-06-12 19:23:01 +0000

    PAC-4 updated app.py, added a hashing function for password hashing

--- dev/app.py
+++ dev/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, url_for
+from hashlib import md5
+from base58 import b58encode
+from db import db, app
 
-app = Flask(__name__)
 
-@app.route('/admin')
-def admin(request):
-    return "This is the admin page"
+def hash_pass(password):
+    addon = 'pn782533hdu04088fdu00234432vrertvvf'
+    return md5(b58encode(password + addon)).digest()
+
+@app.route('/login')
+def login(request):
+    email = request.get('email')
+    password = hash_pass(request.get('password'))
+    return "This is the Login page"
 
 @app.route('/')
 def index():
