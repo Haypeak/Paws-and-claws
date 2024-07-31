98832f3d51f28ae157c1e59b73ec2735b2088bca
Ohenewaah09 <aoetsorblewu@st.ug.edu.gh>
2024-07-31 16:47:05 +0000

    ContectUs ft AppointmentForm: Header Adjustment

---
+++ front-end/src/Components/AppointmentForm/AppointmentFormHeader.jsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+import './AppointmentForm.css'
+
+const AppointmentFormHeader = () =>{
+  return (
+    <>
+    <div className='Home'>
+      <div className='Home-text'>
+   <h1>Book An Appointment</h1>
+   <h3>All Your Pet Need Is Here</h3>
+   <p>Your pet’s health and well-being are our top priority. We provide compassionate and professional care to ensure your furry friends live happy and healthy lives. Explore our services and let us partner with you in caring for your beloved pets. 
+   Trust Paws and Claws to treat your pets like family.
+</p>
+     <button className='btns' >View Profile </button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default AppointmentFormHeader
--- front-end/src/Pages/ContactUsPage.jsx
+++ front-end/src/Pages/ContactUsPage.jsx
@@ -1,11 +1,14 @@
 import ContactUs from '../Components/ContactUs/ContactUs';
+import ContactUsHeader from '../Components/ContactUs/ContactUsHeader';
 import Footer from '../Components/Footer/Footer';
 import Navbar from '../Components/Navbar/Navbar';
+
 
 const AppointmentsPage = () => {
     return (
         <div>
             <Navbar color='#d07322'/>
+            <ContactUsHeader/>
             <ContactUs />
             <Footer />
         </div>
