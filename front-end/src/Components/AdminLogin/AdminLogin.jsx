cc2dbd1c77c136275492a75712b84b03b76806ef
Handson-A <hayelgum@st.ug.edu.gh>
2024-08-22 16:04:59 +0000

    Revert "PAC-43 adminPanel blueprint"
    This reverts commit c0a19fd7517c44ee6c32825662b64c763a7ded13.

--- front-end/src/Components/AdminLogin/AdminLogin.jsx
+++ front-end/src/Components/AdminLogin/AdminLogin.jsx
@@ -1,46 +1,17 @@
 import { useState } from "react";
-import {useNavigate } from "react-router-dom";
-// import axios from "axios";
 import './AdminLogin.css';
-// import Dashboard from "../../AdminPanel/AdminPages/Dashboard/Dashboard"
 
 const AdminLogin = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate(); // Initialize navigate
   
     const handleSubmit = (e) => {
       e.preventDefault();
       // Implement your authentication logic here
-      // console.log('Email:', email);
-      // console.log('Password:', password);
-      navigate('/Admin-dashboard');
+      console.log('Email:', email);
+      console.log('Password:', password);
     };
   
-  //   try {
-  //     // Make a POST request to your login API endpoint
-  //     const response = await axios.post('http://your-api-url/auth/login', {
-  //       username: email,
-  //       password: password,
-  //     });
-
-  //     if (response.status === 200) {
-  //       // Assuming the API returns a token or some success response
-  //       // You can save the token in localStorage or cookies
-  //       localStorage.setItem('adminToken', response.data.token);
-        
-  //       // Navigate to the admin dashboard
-  //       navigate('/admin-dashboard');
-  //     } else {
-  //       setError('Login failed. Please check your credentials.');
-  //     }
-  //   } catch (error) {
-  //     setError('An error occurred. Please try again.');
-  //     console.error('Login error:', error);
-  //   }
-  // };
-
     return (
       <div className="login-container">
         <form className="login-form" onSubmit={handleSubmit}>
@@ -63,7 +34,6 @@ const AdminLogin = () => {
               required 
             />
           </div>
-          {/* {error && <p className="error">{error}</p>} */}
           <button type="submit">Login</button>
         </form>
       </div>
